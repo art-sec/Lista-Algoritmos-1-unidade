db343c7f9beab9939884316d7c157ce213c02b16
art-sec <arthurfoiceiro@gmail.com>
2025-04-30 23:20:12 -0300

    Questão 6 resolvida

---
+++ Questão 6/Questao6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	// Declaração das váriaveis utilizadas no código
+	float pi, raio;
+	// Declara o valor aproximado de pi
+	pi = 3.14;
+	// Inserção do valor do raio da circunfêrencia
+	printf("Insira o raio da circunfêrencia: \n");
+	scanf("%f", &raio);
+	// Exibe o valor da circunfêrencia e calcula o valor simultaneamente
+	printf("O valor da circunfêrencia é: %2f", pi * 2 * raio);
+}
