0f02a50dbed7ea4ac82071a911c33bd08508d70e
art-sec <arthurfoiceiro@gmail.com>
2025-04-30 23:32:51 -0300

    Questão 7 resolvida

---
+++ Questão 7/Questao7.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	// Importação de biblioteca para tornar possível a acentuação de palavras na língua portuguesa
+	setlocale(LC_ALL, "portuguese");
+	// Declaração das váriaveis utilizadas ao decorrer do código
+	int a, b, temporario;
+	// Inserção de valores nas váriaveis
+	printf("Digite o valor de a: \n");
+	scanf("%d", &a);
+	printf("Digite o valor de b: \n");
+	scanf("%d", &b);
+	// Exibe os valores das váriaveis antes da troca dos valores
+	printf("O valor antes da troca:\n");
+	printf("A = %d", a);
+	printf("B = %d", b);
+	// Troca os valores utilizando uma váriavel temporária
+	temporario = a;
+	a = b;
+	b = temporario;
+	// Exibe os valores após a troca de valores das váriaveis
+	printf("O valor após a troca:\n");
+	printf("A = %d", a);
+	printf("B = %d", b);
+}
