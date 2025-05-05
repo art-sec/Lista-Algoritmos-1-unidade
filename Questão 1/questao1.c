e25a39af0dfb092aca58a5209ef14de683e9489d
Arthur Barbosa <arthursantosbarbosa123@gmail.com>
2025-05-05 19:35:18 -0300

    Update questao1.c

--- Questão 1/questao1.c
+++ Questão 1/questao1.c
@@ -3,18 +3,18 @@
 #include <locale.h>
 
 int main() {
-	// Importação de biblioteca para tornar possível a acentuação de palavras na língua portuguesa
+	// ImportaÃ§Ã£o de biblioteca para tornar possÃ­vel a acentuaÃ§Ã£o de palavras na lÃ­ngua portuguesa
 	setlocale(LC_ALL, "portuguese");
-	// Declaração das váriaveis dos dois números inteiros para realizar as operações
-	int numero1, numero2;
-	// Processo de inserção de valores nas váriaveis inteiras
-	printf("Insira o primeiro número: \n");
+	// DeclaraÃ§Ã£o das vÃ¡riaveis dos dois nÃºmeros inteiros para realizar as operaÃ§Ãµes
+	float numero1, numero2;
+	// Processo de inserÃ§Ã£o de valores nas vÃ¡riaveis inteiras
+	printf("Insira o primeiro nÃºmero: \n");
 	scanf("%d", &numero1);
-	printf("Insira o segundo número: \n");
+	printf("Insira o segundo nÃºmero: \n");
 	scanf("%d", &numero2);
-	// Processo de exibição dos cálculos no console do usuário
-	printf("A soma dos dois números é: %d\n", numero1 + numero2);
-	printf("A diferença dos dois números é: %d\n", numero1 - numero2);
-	printf("O produto dos dois números é: %d\n", numero1 * numero2);
-	printf("A divisão dos dois números é: %d\n", numero1 / numero2);
+	// Processo de exibiÃ§Ã£o dos cÃ¡lculos no console do usuÃ¡rio
+	printf("A soma dos dois nÃºmeros Ã©: %d\n", numero1 + numero2);
+	printf("A diferenÃ§a dos dois nÃºmeros Ã©: %d\n", numero1 - numero2);
+	printf("O produto dos dois nÃºmeros Ã©: %d\n", numero1 * numero2);
+	printf("A divisÃ£o dos dois nÃºmeros Ã©: %d\n", numero1 / numero2);
 }
